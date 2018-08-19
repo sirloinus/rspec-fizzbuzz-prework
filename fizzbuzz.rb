37103825bcdfdbc48d6801de966f09203b69adfe
Amalie S <amaliesmidth@gmail.com>
2018-08-19 16:32:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    print 'Fizz'
+    return 'Fizz'
   elsif int % 5 == 0
-    print 'Buzz'
+    return 'Buzz'
   elsif int % 3 && int % 5
-    print 'Fizzbuzz'
-  else print 'nill'
+    return 'Fizzbuzz'
+  else return 'nill'
   end
 end
