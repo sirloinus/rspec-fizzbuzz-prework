b76581347d3eb478d3f31650fb23b1e2d30213f9
Amalie S <amaliesmidth@gmail.com>
2018-08-19 16:42:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     return 'Buzz'
   elsif int % 3 && int % 5
     return 'FizzBuzz'
-  elsif int % 3 !== 0 && int % 5 !== 0
+  elsif int % 3 != 0 && int % 5 != 0
     return 'nil'
   end
 end
